694f19c61f7307eea484c750b7974cca182d2f7b
Jens Okneild <jens.oknelis@gmail.com>
2015-12-18 13:23:46 +0100

    Added js for android

--- MaskedView.android.js
+++ MaskedView.android.js
@@ -1,11 +1,46 @@
+/**
+ * @providesModule MaskedView
+ * @flow
+ */
+
 'use strict';
 
-var warning = require('warning');
+var React = require('react-native');
+var {
+  requireNativeComponent,
+  PropTypes,
+} = React;
 
-var MaskedView = {
-  test: function() {
-    warning("Not yet implemented for Android.");
-  }
-};
+var resolveAssetSource = require('../react-native/Libraries/Image/resolveAssetSource');
+
+var MaskedView = React.createClass({
+  propTypes: {
+    /**
+     * `uri` is a string representing the resource identifier for the image, which
+     * could be an http address, a local file path, or the name of a static image
+     * resource (which should be wrapped in the `require('image!name')` function).
+     */
+    maskImage: PropTypes.oneOfType([
+      PropTypes.shape({
+        uri: PropTypes.string,
+      }),
+      // Opaque type returned by require('./image.jpg')
+      PropTypes.number,
+    ]),
+  },
+
+  render() {
+    var imgSource = resolveAssetSource(this.props.maskImage);
+    return (
+      <NativeMaskedView
+        {...this.props}
+        maskImage={imgSource}
+      />
+    );
+  },
+
+});
+
+var NativeMaskedView = requireNativeComponent('RNMaskedViewAndroid', MaskedView);
 
 module.exports = MaskedView;

cab87bc732633f3c709a1cdd0dace2c205821c66
Konrad Beckmann <konrad.beckmann@gmail.com>
2016-02-04 15:28:40 +0100

    Updated to use annotated ReactProps (react-native 0.19 requirement)

--- src/main/java/com/rnmaskedview/RNMaskedViewManager.java
+++ src/main/java/com/rnmaskedview/RNMaskedViewManager.java
@@ -5,7 +5,7 @@ import javax.annotation.Nullable;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.ReactProp;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.common.annotations.VisibleForTesting;
 
 public class RNMaskedViewManager extends ViewGroupManager<RNMaskedView> {
